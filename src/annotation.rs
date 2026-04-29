53a51ab0a2070e28efee0551546c85e99d175014
Gabriel Hideki Izuka Moraes <gabriel.hideki@hotmail.com>
2026-04-28 21:54:28 -0300

    perf: walk through datasets linearly

--- src/annotation.rs
+++ src/annotation.rs
@@ -4,7 +4,6 @@ use rust_htslib::bcf::{
 };
 use rust_htslib::errors::Error::GenomicSeek;
 use serde_json::Value;
-use std::collections::HashMap;
 
 use crate::json::{format_variant_json, write_json_output};
 use crate::models::{AnnotatedVariant, AnnotationRecord, Variant, VcfDataset};
@@ -26,6 +25,14 @@ fn extract_contigs(header_view: &HeaderView) -> Vec<String> {
         }
     }
     contigs
+}
+
+fn move_to_next_record(reader: &mut IndexedReader) -> Option<Record> {
+    let mut record = reader.empty_record();
+    match reader.read(&mut record) {
+        Some(Ok(_)) => Some(record),
+        _ => None,
+    }
 }
 
 pub fn annotate_single_variant(variant: &Variant, vcfs: &Vec<VcfDataset>) -> AnnotatedVariant {
@@ -50,10 +57,10 @@ pub fn annotate_single_variant(variant: &Variant, vcfs: &Vec<VcfDataset>) -> Ann
         for vcf_record in reader.records() {
             let record = vcf_record.expect("Failed to read VCF dataset record.");
             let alleles = extract_alleles(&record);
-            let ref_allele = alleles[0].clone();
+            let ref_allele = &alleles[0];
             let alt_alleles = &alleles[1..];
             if variant.position == record.pos() as u64
-                && variant.ref_allele == ref_allele
+                && ref_allele == &variant.ref_allele
                 && alt_alleles.contains(&variant.alt_allele)
             {
                 let info_tags = extract_tags_from_record(&record, &vcf.tag_names);
@@ -81,9 +88,13 @@ pub fn annotate_vcf(
     keep_records: bool,
     output: String,
 ) {
-    let mut vcf_readers: Vec<(&VcfDataset, IndexedReader)> = vcfs
+    let mut vcf_readers: Vec<(&VcfDataset, IndexedReader, Option<Record>)> = vcfs
         .iter()
-        .map(|vcf| (vcf, load_vcf(&vcf.file_path)))
+        .map(|vcf| {
+            let mut reader = load_vcf(&vcf.file_path);
+            let first_record = move_to_next_record(&mut reader);
+            (vcf, reader, first_record)
+        })
         .collect();
 
     let chromosomes = extract_contigs(input_reader.header());
@@ -92,50 +103,10 @@ pub fn annotate_vcf(
     let mut append = false;
 
     for chrom in chromosomes {
-        let mut datasets_cache: Vec<(String, HashMap<(u64, String, String), AnnotationRecord>)> =
-            Vec::new();
-
-        for (vcf, vcf_reader) in vcf_readers.iter_mut() {
-            let rid = match vcf_reader.header().name2rid(chrom.as_bytes()) {
-                Ok(r) => r,
-                Err(_) => continue,
-            };
-
-            match vcf_reader.fetch(rid, 0, None) {
-                Ok(_) => (),
-                Err(e) => match e {
-                    GenomicSeek {
-                        contig: _,
-                        start: _,
-                    } => continue,
-                    _ => panic!("{}", e),
-                },
-            }
-
-            let mut records_map: HashMap<(u64, String, String), AnnotationRecord> = HashMap::new();
-            for vcf_record in vcf_reader.records() {
-                let record = vcf_record.expect("Failed to read VCF dataset record.");
-                let tag_annotations = extract_tags_from_record(&record, &vcf.tag_names);
-                let alleles = extract_alleles(&record);
-                let ref_allele = alleles[0].clone();
-                for alt in &alleles[1..] {
-                    let record_key = (record.pos() as u64, ref_allele.to_string(), alt.to_string());
-                    let annotation_record = AnnotationRecord {
-                        record_id: String::from_utf8(record.id()).unwrap(),
-                        info_tags: tag_annotations.clone(),
-                    };
-                    records_map.insert(record_key, annotation_record);
-                }
-            }
-
-            datasets_cache.push((vcf.get_dataset_name(), records_map));
-        }
-
         let rid = match input_reader.header().name2rid(chrom.as_bytes()) {
             Ok(r) => r,
             Err(_) => continue,
         };
-
         match input_reader.fetch(rid, 0, None) {
             Ok(_) => (),
             Err(e) => match e {
@@ -156,23 +127,56 @@ pub fn annotate_vcf(
             } else {
                 None
             };
-            let position = record.pos();
             let alleles = extract_alleles(&record);
-            let ref_allele = alleles[0].clone();
+            let ref_allele = &alleles[0];
             for alt in &alleles[1..] {
-                let variant_str = format!("{}:{}:{}:{}", chrom, position + 1, &ref_allele, alt);
+                let variant_str = format!("{}:{}:{}:{}", chrom, record.pos() + 1, ref_allele, alt);
                 let variant = Variant::new(variant_str);
 
                 let mut annotation_records: Vec<(String, AnnotationRecord)> = Vec::new();
-                for (vcf_basename, records_map) in &datasets_cache {
-                    let record_key = (
-                        variant.position,
-                        variant.ref_allele.clone(),
-                        variant.alt_allele.clone(),
-                    );
-                    if records_map.contains_key(&record_key) {
-                        let dataset_record = records_map.get(&record_key).unwrap();
-                        annotation_records.push((vcf_basename.clone(), dataset_record.clone()));
+
+                for (vcf_dataset, reader, curr_record) in vcf_readers.iter_mut() {
+                    loop {
+                        match curr_record {
+                            Some(r) => {
+                                let curr_chrom = {
+                                    let curr_rid = r.rid().unwrap();
+                                    String::from_utf8(
+                                        reader.header().rid2name(curr_rid).unwrap().to_vec(),
+                                    )
+                                    .unwrap()
+                                };
+                                if curr_chrom == chrom && r.pos() == record.pos() {
+                                    let curr_alleles = extract_alleles(&r);
+                                    let curr_ref_allele = &curr_alleles[0];
+                                    let curr_alt_alleles = &curr_alleles[1..];
+                                    if curr_ref_allele == &variant.ref_allele
+                                        && curr_alt_alleles.contains(&variant.alt_allele)
+                                    {
+                                        let annotation_record = AnnotationRecord {
+                                            record_id: String::from_utf8(r.id()).unwrap(),
+                                            info_tags: extract_tags_from_record(
+                                                &r,
+                                                &vcf_dataset.tag_names,
+                                            ),
+                                        };
+                                        annotation_records.push((
+                                            vcf_dataset.get_dataset_name(),
+                                            annotation_record,
+                                        ));
+                                        *curr_record = move_to_next_record(reader);
+                                        break;
+                                    } else {
+                                        *curr_record = move_to_next_record(reader);
+                                    };
+                                } else if r.pos() > record.pos() {
+                                    break;
+                                } else {
+                                    *curr_record = move_to_next_record(reader);
+                                }
+                            }
+                            None => break,
+                        }
                     }
                 }
 
